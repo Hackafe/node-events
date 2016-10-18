46ab071ffc47bd5fd3bdf91666ef1eeac823d454
Geno Roupsky <geno@roupsky.name>
2016-10-18 14:38:26 +0300

    package to retrieve upcomming events of hackafe

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    apiKey: 'd5c03d2f89476c7f77dc5f5c4d2fbe00',
+    boardId: 'GHda069L'
+};
---
+++ index.js
@@ -0,0 +1,95 @@
+var debug = require('debug')('hackafe-events');
+var config = require('./config');
+var Promise = require('promise');
+var get = require('simple-get');
+
+var FACEBOOK_EVENT_PATTERN = /((https?:)?\/\/(www.)?facebook\.com\/events\/\d+)/i;
+var FORUM_THREAD_PATTERN = /((https?:)?\/\/frm\.hackafe\.org\/t\/[-a-zA-Z0-9]+\/\d+)/i;
+
+var labelCache = {};
+
+module.exports = function (opts, cb) {
+    if (typeof opts === 'function') {
+        cb = opts;
+        opts = {};
+    }
+
+    return getBoardVisibleCards(config.boardId)
+        .then(function (cards) {
+            return Promise.all(cards.map(transform));
+        })
+        .then(function (events) {
+            return events.filter(upcomming);
+        })
+        .nodeify(cb);
+};
+
+function tfetch(opts) {
+    if (typeof opts === 'string') opts = {url: opts};
+    opts.url += '?key=' + config.apiKey;
+    opts.json = true;
+    return new Promise(function (resolve, reject) {
+        debug('fetching %s', opts.url);
+        get.concat(opts, function (err, res, data) {
+            if (err) return reject(err);
+            debug('fetched %s', opts.url);
+            resolve(data);
+        });
+    });
+}
+
+function getBoardVisibleCards(boardId) {
+    return tfetch('https://api.trello.com/1/boards/' + config.boardId + '/cards/visible');
+}
+
+function getCardAttachment(cardId, attachmentId) {
+    return tfetch('https://api.trello.com/1/cards/' + cardId + '/attachments/' + attachmentId);
+}
+
+function getLabel(labelId) {
+    return labelCache[labelId] = labelCache[labelId] || tfetch('https://api.trello.com/1/labels/' + labelId);
+}
+
+function transform(card) {
+    return Promise.resolve({
+            id: card.id,
+            desc: card.desc,
+            name: card.name,
+            start: card.due && new Date(card.due),
+            url: card.url
+        })
+        .then(function fetchCover(event) {
+            if (!card.idAttachmentCover) return event;
+            return getCardAttachment(card.id, card.idAttachmentCover)
+                .then(function (attachment) {
+                    event.cover = attachment.url;
+                    return event;
+                })
+        })
+        .then(function extractFacebookEvent(event) {
+            var match = event.desc &&
+                event.desc.match(FACEBOOK_EVENT_PATTERN);
+            if (match)
+                event.facebookEvent = match[0];
+            return event;
+        })
+        .then(function extractForumThread(event) {
+            var match = event.desc &&
+                event.desc.match(FORUM_THREAD_PATTERN);
+            if (match)
+                event.forumThread = match[0];
+            return event;
+        })
+        .then(function resolveLabels(event) {
+            if (!card.idLabels) return event;
+            return Promise.all(card.idLabels.map(getLabel))
+                .then(function (labels) {
+                    event.labels = labels;
+                    return event;
+                });
+        })
+}
+
+function upcomming(event) {
+    return event.start && event.start.getTime() >= new Date().getTime();
+}
